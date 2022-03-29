f90cec6e6f492ab724f1e9150767a5daeef2553a
Hung <nguyenkhachung0708@gmail.com>
2022-03-29 16:09:17 +0700

    done

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập năm cần kiểm tra : ");
+        int year = scanner.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println( year + " là năm nhuận ");
+                } else {
+                    System.out.println( year + " không là năm nhuận ");
+                }
+            } else {
+                System.out.println( year + " là năm nhuận ");
+            }
+        } else {
+            System.out.println( year + " không là năm nhuận ");
+        }
+    }
+}
